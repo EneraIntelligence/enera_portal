b3a6af1e909370da5aa5837332f55f3337dd4022
Jose Asdrubal Rosas Matus <arosas@enera.mx>
2015-09-29 16:13:29 -0500

    jquery

--- resources/views/layouts/interaction.blade.php
+++ resources/views/layouts/interaction.blade.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
     <title>@yield('title')</title>
+    <meta charset="UTF-8">
+    {{--<meta name="viewport" content="width=device-width, initial-scale=1" >--}}
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('css/bootstrap.css') }}" rel="stylesheet">
+    <script language="JavaScript" type="text/javascript" src="{{ URL::asset('js/jquery.min.js') }}" ></script>
+
+
     @yield('head_scripts')
+
 </head>
 <body>
 @yield('content')
