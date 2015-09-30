8ab46219098c40095dc6a85c0adda624b1a76059
Eder Díaz <ederchrono@gmail.com>
2015-09-30 15:56:18 -0500

    Added mailing list blade and its test route

--- resources/views/interaction/mailingList.blade.php
+++ resources/views/interaction/mailingList.blade.php
@@ -1,18 +1,24 @@
 @extends('layouts.interaction')
 
 @section('content')
-    <div class=" container">
-        <div class="center-block">
-            <img class="img-responsive center-block"
-                 src="{{asset('img').'/'.$data['imagen'] }}"
-                 alt="Enera Portal">
-        </div>
+
+    <div class="center-block container">
+
+        <!-- banner -->
+        <img class="img-responsive center-block"
+             src="{{asset('img').'/'.$data['imagen'] }}"
+             alt="Enera Portal">
+
+        <!-- subscribe button -->
         <button id="subscribe" type="button" class="btn btn-primary btn-block" data="{{$data['link']}}">
             Deseo suscribirme
         </button>
+
+        <!-- navigate button -->
         <a id="navigate" href="#" data="{{$data['link']}}">
             <p class="text-center">Navegar en internet</p>
         </a>
+
     </div>
 
 @stop
@@ -22,12 +28,13 @@
     <script>
 
         $(function(){
+            //jquery is loaded
 
             //setup clicks
             $("#subscribe").click(subscribe);
             $("#navigate").click(navigate);
 
-        });//jquery is loaded
+        });
 
         function subscribe()
         {
