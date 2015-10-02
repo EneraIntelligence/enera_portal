73a781b0adee328df338afee9db04f032c3e0f46
Alfonso Rodriguez <alfonso@vizinova.com>
2015-10-02 14:02:41 -0500

    Test, fixed background welcome view image

--- app/Libraries/CampaignSelector.php
+++ app/Libraries/CampaignSelector.php
@@ -35,17 +35,18 @@ class CampaignSelector
     private function selector()
     {
         // TODO aqui la consulta para obtener la(s) campaña(s) adecuada(s) al usuario
-        $fb_date = explode('-',$this->user['facebook']['birthday']['date']);
+        $date = explode(' ',$this->user['facebook']['birthday']['date']);
+        $fb_date = explode('-',$date[0]);
         $birthday = new DateTime($fb_date[0].'-'.$fb_date[1].'-'.$fb_date[2]);
-        $campaign =  Campaign::whereIn('filter.age',[$birthday->diff(new DateTime(date('Y-m-d')))])
+        $campaign =  Campaign::whereIn('filter.age',[$birthday->diff(new DateTime(date('Y')))])
             ->where( 'filter.date.start', '>', new DateTime(date('Y-m-d')))
             ->where( 'filter.date.end','<', new DateTime(date('Y-m-d')))
-            ->whereIn('filter.week_days',[date('w')])->whereIn('filter.day_hours', [date('h')])
+            ->whereIn('filter.week_days',[date('w')])->whereIn('filter.day_hours', [date('H')])
             ->whereIn('filter.gender', [$this->user['facebook']['gender']])->where('status', 'active')
             ->orderBy('balance', 'desc')
             ->get();
 
-        $campaign =  Campaign::all();
+       $campaign = $birthday->diff(new DateTime(date('Y-m-d')));
         return $campaign;
     }
 }
