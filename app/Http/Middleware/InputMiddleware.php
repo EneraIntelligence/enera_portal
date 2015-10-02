16131afce9dbcb307eeef289d949f27663f4d1ba
Pedro Luna <pluna@enera.mx>
2015-10-02 15:44:25 -0500

    new middleware: InputMiddleware

---
+++ app/Http/Middleware/InputMiddleware.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Portal\Http\Middleware;
+
+use Closure;
+
+class InputMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+}
