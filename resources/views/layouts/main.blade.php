f73c28376b8fcf70075a7dd65b826b3b2daa9c34
Alfonso Rodriguez <alfonso@vizinova.com>
2015-10-06 13:08:45 -0500

    view rotation test

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -20,7 +20,6 @@
 
 <div id="modal1" class="modalmask">
     <div class="modalbox movedown">
-        <a href="#close" title="Close" class="close">X</a>
         <h2>DESLIZAR</h2>
         <p>La ventana modal aparece por arriba y se desliza hasta su posición. Un efecto simple pero elegante.</p>
         <p>Aquí puedes incluir cualquier cosa como vídeos, mapas, formularios...</p>
