675168e80d71f532f58bba20663945fedb04bf7c
Alfonso Rodriguez <alfonso@vizinova.com>
2015-10-06 10:36:53 -0500

    view rotation test

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     window.addEventListener("orientationchange", function() {
         // Announce the new orientation number
-        if(window.orientation == 90){
+        if(window.orientation == 0){
             alert('portrait mode');
         }
-        else
+        else if (window.orientation == 90)
         {
             alert('landscape mode ');
         }
