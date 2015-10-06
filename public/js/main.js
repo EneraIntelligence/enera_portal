5d50d87433c8791b1be9fd89e3e5d59db7dc93f0
Alfonso Rodriguez <alfonso@vizinova.com>
2015-10-06 10:35:04 -0500

    view rotation test

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
     window.addEventListener("orientationchange", function() {
         // Announce the new orientation number
-        alert(window.orientation);
+        if(window.orientation == 90){
+            alert('portrait mode');
+        }
+        else
+        {
+            alert('landscape mode ');
+        }
     }, false);
 })
