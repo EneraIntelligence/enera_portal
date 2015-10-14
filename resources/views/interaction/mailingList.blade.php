108b51f4d604038c6d3a7650e85fc0945b17509e
Eder Díaz <ederchrono@gmail.com>
2015-10-14 10:01:25 -0500

    changed received variables

--- resources/views/interaction/mailingList.blade.php
+++ resources/views/interaction/mailingList.blade.php
@@ -11,41 +11,29 @@
     <div>
         <!-- banner -->
         <img class="img-responsive center-block"
-             src="{{asset('img').'/'.$data['imagen'] }}"
+             src="{!! $image !!} }}"
              id="banner-mailing"
              alt="Enera Portal">
 
-        {{--
-        <img class="img-landscape img-responsive center-block banner"
-             src="{{asset('img').'/banner-hor.png' }}"
-             id="banner-horizontal"
-             alt="Enera Portal">
-        --}}
     </div>
 
     <div class="banner-button">
         <!-- subscribe button -->
-        <button id="subscribe" type="button" class="btn btn-primary btn-block" susses_url="{{Input::get('base_grant_url').'?continue_url='.Input::get('user_continue_url').'&duration=900' }}">
+        <button id="subscribe" type="button" class="btn btn-primary btn-block"
+                success_url="{{Input::get('base_grant_url').'?continue_url='.Input::get('user_continue_url').'&duration=900' }}">
             SUSCRIBIRME
         </button>
 
         <!-- navigate button -->
         <div style="margin: 10px 0;">
-            <a id="navegar" href="#" susses_url="{{Input::get('base_grant_url').'?continue_url='.Input::get('user_continue_url').'&duration=900' }}">
+            <a id="navegar" href="#"
+               success_url="{{Input::get('base_grant_url').'?continue_url='.Input::get('user_continue_url').'&duration=900' }}">
                 <p class="text-center">Deseo navegar en internet sin suscribirme</p>
             </a>
         </div>
     </div>
 
-    {{--<div class="banner-button">--}}
-        {{--<div>--}}
-            {{--<button id="navegar" type="button" class="btn btn-primary btn-block" data="{{$data['link']}}"> Navegar en--}}
-                {{--internet--}}
-            {{--</button>--}}
-        {{--</div>--}}
 
-    {{--</div>--}}
-w
 @stop
 
 @section('footer_scripts')
@@ -53,8 +41,8 @@ w
     <script>
         $(document).ready(function () {
             var myLog = new logs();
-            console.log("ready!");
-            console.log($("#navegar").attr('susses_url'));
+            //console.log("ready!");
+            //console.log($("#navegar").attr('success_url'));
             myLog.loaded({
                 _token: "{!! session('_token') !!}",
                 client_mac: "{!! Input::get('client_mac') !!}"
@@ -67,7 +55,7 @@ w
                 myLog.saveMail({
                     _token: "{!! session('_token') !!}"
                 });
-                myLog.redirectOut(btn.attr('susses_url'));
+                myLog.redirectOut(btn.attr('success_url'));
                 {{--window.location.href = "{!! route("campaign::action::save_mail") !!}";--}}
 
             });
@@ -75,11 +63,11 @@ w
             btn.click(function () {
                 console.log('click en el boton');
 //                console.log();
-                var response =myLog.completed({
+                var response = myLog.completed({
                     _token: "{!! session('_token') !!}",
                     client_mac: "{!! Input::get('client_mac') !!}"
                 });
-                myLog.redirectOut(btn.attr('susses_url'));
+                myLog.redirectOut(btn.attr('success_url'));
             });
         });
     </script>
