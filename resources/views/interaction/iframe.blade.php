9ff522cc24eecf281ce251f8e65d2b2adc185e93
Jose Asdrubal Rosas Matus <arosas@enera.mx>
2015-10-20 09:22:17 -0500

    se modifico la url del iframe

--- resources/views/interaction/iframe.blade.php
+++ resources/views/interaction/iframe.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <div style="width:100%; height:90%;">
-        <iframe src="https://www.blablacar.mx/registro-pasajero?comuto_cmkt=MX_ADWORDSBRAND&utm_source=ADWORDSBRAND&utm_campaign=MX_ADWORDSBRAND&gclid=Cj0KEQjwqZKxBRDBkNmLt9DejNgBEiQAq8XWPoTqwI7uazQKZH1vu1cFmL-RuiPf-c0qNjY8Nv2bpKQaAmKZ8P8HAQ" frameborder="0" style="width:100%; height:100%;"></iframe>
+        <iframe src="https://www.blablacar.mx" frameborder="0" style="width:100%; height:100%;"></iframe>
     </div>
     <div class="banner-button">
         <div>
