e11b09c5b1fffe0c1c56fcd5ab5922e8be282fdd
Jose Asdrubal Rosas Matus <arosas@enera.mx>
2015-10-20 09:30:49 -0500

    se modifico la url del iframe**

--- resources/views/interaction/iframe.blade.php
+++ resources/views/interaction/iframe.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <div style="width:100%; height:90%;">
-        <iframe src="https://www.blablacar.mx" frameborder="0" style="width:100%; height:100%;"></iframe>
+        <iframe src="{{$data['iframe']}}" frameborder="0" style="width:100%; height:100%;"></iframe>
     </div>
     <div class="banner-button">
         <div>
