b6a8448cf2248f6b0ee0cc72bb925f617d90841c
Alfonso Rodriguez <alfonso@vizinova.com>
2015-12-21 13:45:04 -0600

    Video interaction

--- app/Http/Controllers/CampaignsController.php
+++ app/Http/Controllers/CampaignsController.php
@@ -41,21 +41,7 @@ class CampaignsController extends Controller
             'user_id' => $user_id
         ]));
 
-        $agent = new Agent();
-        if($agent->is('iPhone'))
-        {
-            $os = 'Iphone';
-        }elseif($agent->is('Android'))
-        {
-            $os = 'Android';
-        }elseif($agent->is('OS X'))
-        {
-            $os = 'mac';
-        }else{
-            $os = 'Dipositivo no detectado';
-        }
-//        dd($campaignSelected->_id);
-        return view($interaction->getView(), array_merge(['_id' => $campaignSelected->_id], $interaction->getData(), ['os' =>$os]));
+        return view($interaction->getView(), array_merge(['_id' => $campaignSelected->_id], $interaction->getData()));
 
     }
 
