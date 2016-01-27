8535a3537690fcc2f1d21fb12ef14ef36eab8223
Eder Díaz <ederchrono@gmail.com>
2016-01-27 13:43:44 -0600

    cambio en facepas

--- resources/views/interaction/facePas.blade.php
+++ resources/views/interaction/facePas.blade.php
@@ -5,7 +5,7 @@
 //        var myLog = new logs();
         console.log("ready!");
 
-        window.location.href="{{Input::get('base_grant_url').'?continue_url=http://'.Input::get('user_continue_url').'&duration='.session('session_time')}}"
+        window.location.href="{{Input::get('base_grant_url').'?continue_url='.Input::get('user_continue_url').'&duration='.session('session_time')}}"
 
     });
 </script>
