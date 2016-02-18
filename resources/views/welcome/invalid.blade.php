daa5ed676d72f3c84f77488556d1719e3e340b16
Eder Díaz <ederchrono@gmail.com>
2016-02-18 15:56:12 -0600

    Mostrando parametros de open-mesh en invalid portal

--- resources/views/welcome/invalid.blade.php
+++ resources/views/welcome/invalid.blade.php
@@ -5,4 +5,17 @@
 @section('main_bg'){!! $main_bg !!}@stop
 @section('content')
     Invalid Network
+
+    <pre>
+        res: {{$_GET["res"]}}
+        uamip: {{$_GET["uamip"]}}
+        uamport: {{$_GET["uamport"]}}
+        mac: {{$_GET["mac"]}}
+        called: {{$_GET["called"]}}
+        ssid: {{$_GET["ssid"]}}
+        nasid: {{$_GET["nasid"]}}
+        userurl: {{$_GET["userurl"]}}
+        challenge: {{$_GET["challenge"]}}
+    </pre>
+
 @stop
