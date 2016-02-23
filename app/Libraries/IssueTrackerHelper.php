a28c8d1226bf3184c973934f4798597f6ee684f4
Pedro Luna <pluna@enera.mx>
2016-02-23 15:47:31 -0600

    [FIX] IssueTrackerHelper

--- app/Libraries/IssueTrackerHelper.php
+++ app/Libraries/IssueTrackerHelper.php
@@ -58,7 +58,7 @@ class IssueTrackerHelper
             ],
             'file' => [
                 'line' => $e->getLine(),
-                'path' => $e->getFile(),
+                'path' => str_replace(base_path(), '', $e->getFile()),
                 'context' => $context,
             ],
             'exception' => [
