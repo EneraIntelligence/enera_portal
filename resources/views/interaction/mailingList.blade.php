3edbcb3d8ec36251cea58ea26c03516761a00950
Alfonso Rodriguez <alfonso@vizinova.com>
2016-02-26 13:29:00 -0600

    fix to button mailing_list size button

--- resources/views/interaction/mailingList.blade.php
+++ resources/views/interaction/mailingList.blade.php
@@ -29,10 +29,10 @@
             {{--ME INTERESA--}}
         {{--</button>--}}
 
-        <div id="subscribe" class="col-md-12  col-xs-12 e-content" style="cursor: pointer;"
+        <div id="subscribe" class="col-md-12  col-xs-12 e-content" style="cursor: pointer; margin: 0 0 15px 0"
              success_url="{{Input::get('base_grant_url').'?continue_url='.Input::get('user_continue_url').'&duration='. session('session_time') }}">
             <div class="col-md-9 col-xs-9 button-text">
-                <p style="margin: 0; font-size: larger;">ME INTERESA</p>
+                <p style="margin: 0; font-size: 135%;">ME INTERESA</p>
             </div>
             <div class="col-md-3  col-xs-3 button-icon">
                 <div class="borderLeft">
