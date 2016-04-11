dc95469fd6fc8b020937c71cff7d639a086d935b
Pedro Luna <pluna@enera.mx>
2016-04-11 17:29:08 -0500

    [FIX] Cambios a IssueTracker y sus modelos para agrupar issues

--- app/Issue.php
+++ app/Issue.php
@@ -6,12 +6,23 @@ use Jenssegers\Mongodb\Model as Model;
 
 class Issue extends Model
 {
-    protected $fillable = ['msg', 'request', 'file', 'exception', 'responsible_id', 'priority', 'status', 'history'];
+    protected $fillable = ['issue', 'exception', 'statistic', 'recurrence', 'responsible_id', 'priority', 'status', 'history'];
+    protected $dates = ['created_at', 'updated_at'];
 
     // relations
     public function reponsible()
     {
         return $this->belongsTo('Portal\Administrator', 'responsible_id');
+    }
+
+    public function recurrence()
+    {
+        return $this->embedsMany('Portal\IssueRecurrence');
+    }
+
+    public function statistic()
+    {
+        return $this->embedsMany('Portal\IssueStatistic');
     }
     // end relations
 }
