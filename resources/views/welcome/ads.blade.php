46fe73f08e7f98cab9b24550a5856aa13590eda1
Eder Díaz <ederchrono@gmail.com>
2016-04-11 15:27:38 -0500

    facepas a ads - 5

--- resources/views/welcome/ads.blade.php
+++ resources/views/welcome/ads.blade.php
@@ -4,13 +4,25 @@
 @stop
 @section('main_bg'){!! $main_bg !!}@stop
 @section('content')
-
+        <!--
     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- Ads H-26-50 -->
+     Ads H-26-50
     <ins class="adsbygoogle"
          style="display:block"
          data-ad-client="ca-pub-7906422245182015"
          data-ad-slot="6643903287"
+         data-ad-format="auto"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+-->
+
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!— Anuncio Adaptable —>
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-7906422245182015"
+         data-ad-slot="2591062884"
          data-ad-format="auto"></ins>
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({});
