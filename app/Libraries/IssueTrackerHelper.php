1fca25b2676a9ca6542b4e1103e9b70d05e41312
Pedro Luna <pluna@enera.mx>
2016-04-20 16:49:19 -0500

    [Testing]

--- app/Libraries/IssueTrackerHelper.php
+++ app/Libraries/IssueTrackerHelper.php
@@ -71,8 +71,9 @@ class IssueTrackerHelper
                 ]
             ]);
 
-            $issue_statistic = $issue->statistic;
+            $issue_statistic = $issue->statistic()
                 //->where('date', new MongoDate(strtotime(date('Y-m-d') . 'T00:00:00-0600')))->first();
+                ->where('host', 'exists', 'BERYLLIUM-0')->first();
 
             dd($issue_statistic);
 
