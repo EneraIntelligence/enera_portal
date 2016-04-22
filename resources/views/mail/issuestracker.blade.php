7a0c18f7641dd167f0fcb34a937f32d626b62065
Pedro Luna <pluna@enera.mx>
2016-04-22 18:00:24 -0500

    [New] Send mail when create a new issue*

--- resources/views/mail/issuestracker.blade.php
+++ resources/views/mail/issuestracker.blade.php
@@ -1,44 +1,6 @@
 <table>
     <tr>
         <td><b>Time:</b></td>
-        <td>{{ $time }}</td>
-    </tr>
-    <tr>
-        <td><b>Host:</b></td>
-        <td>{{ gethostname() }}</td>
-    </tr>
-    <tr>
-        <td><b>Path:</b></td>
-        <td>{{ $request->path() }}</td>
-    </tr>
-    <tr>
-        <td><b>Url:</b></td>
-        <td>{{ $request->url() }}</td>
-    </tr>
-    <tr>
-        <td><b>Code:</b></td>
-        <td>{{ $ex->getCode() }}</td>
-    </tr>
-    <tr>
-        <td><b>File:</b></td>
-        <td>{{ $ex->getFile() }}</td>
-    </tr>
-    <tr>
-        <td><b>Line:</b></td>
-        <td>{{ $ex->getLine() }}</td>
-    </tr>
-    <tr>
-        <td><b>Message:</b></td>
-        <td>{{ $ex->getMessage() }}</td>
-    </tr>
-    <tr>
-        <td><b>Trace:</b></td>
-        <td>{{ $ex->getTraceAsString() }}</td>
-    </tr>
-    <tr>
-        <td><b>Session vars:</b></td>
-        <td>
-            {!! json_encode($session_vars) !!}
-        </td>
+        <td>{{ $issue->issue['title']}}</td>
     </tr>
 </table>
