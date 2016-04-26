1709cd4b9d9b331f803d4c84a026e45859d436ac
Pedro Luna <pluna@enera.mx>
2016-04-26 12:35:13 -0500

    [New] Send mail when create a new issue*

--- resources/views/mail/issuestracker.blade.php
+++ resources/views/mail/issuestracker.blade.php
@@ -7,27 +7,43 @@
     <link rel="stylesheet" href="">
 </head>
 <body style="margin:0;">
-<header >
+<header>
     <div style="background-color: #00BFFF; padding: 20px; text-align:center;">
         <h1 style="margin: 0 auto; color: white;">Issues Traker</h1>
     </div>
 </header>
 
 <main style="padding: 0 50px;">
-    <p><font size="9" color="#696969">New exception in enera_portal</font></p>
+    <p>
+        <font size="9" color="#696969">
+            New exception in Enera {{ $issue->issue['platform'] }}
+        </font>
+    </p>
     <br>
-    <p><font size="4" color="#000000">InvalidArgumentException</font>
-        <font size="4" color="#696969"> - vendor/laravel/framework/src/Illuminate/Database/DatabaseManager.php:238</font></p>
-    <p><font size="3" color="#696969">IDatabase [enera] not configured./font></p>
+    <p>
+        <font size="4" color="#000000">
+            {{ $issue->issue['title'] }}
+        </font>
+        <font size="4" color="#696969">
+            - {{ $issue->issue['file']['path'] }}:{{ $issue->issue['file']['line'] }}
+        </font>
+    </p>
+    <p>
+        <font size="3" color="#696969">
+            {{ $issue->exception['msg'] }}
+        </font>
+    </p>
     <br>
     <p><font size="4" color="#696969">STAGE</font></p>
     <p><font size="3" color="#696969">production</font></p>
     <p><font size="4" color="#696969">SEVERITY</font></p>
-    <p><font size="" color="#696969">error</font></p>
+    <p><font size="3" color="#696969">error</font></p>
     <br>
     <p><font size="9" color="#696969">Stacktrace summary</font></p>
-    <p style="border: solid #696969 1px; background-color: #f0f0f0; padding:5px 20px; margin:0;">app/Jobs/FbLikesJob.php:55 · [main]</p>
-    <p style="border: solid #696969 1px; background-color: #f0f0f0; padding:5px 20px; margin:0;">app/Jobs/FbLikesJob.php:55 · [main]</p>
+    <p style="border: solid #696969 1px; background-color: #f0f0f0; padding:5px 20px; margin:0;">
+        app/Jobs/FbLikesJob.php:55 · [main]</p>
+    <p style="border: solid #696969 1px; background-color: #f0f0f0; padding:5px 20px; margin:0;">
+        app/Jobs/FbLikesJob.php:55 · [main]</p>
 </main>
 
 </body>
