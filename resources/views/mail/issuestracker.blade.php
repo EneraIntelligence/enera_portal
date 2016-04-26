72063098f16e109c8a9874838ca328766821b6b1
Alfonso Rodriguez <alfonso@vizinova.com>
2016-04-26 11:57:50 -0500

    new view of email issuestraker blade

--- resources/views/mail/issuestracker.blade.php
+++ resources/views/mail/issuestracker.blade.php
@@ -1,6 +1,34 @@
-<table>
-    <tr>
-        <td><b>Time:</b></td>
-        <td>{{ $issue->issue['title']}}</td>
-    </tr>
-</table>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <link rel="stylesheet" href="">
+</head>
+<body style="margin:0;">
+<header >
+    <div style="background-color: #00BFFF; padding: 20px; text-align:center;">
+        <h1 style="margin: 0 auto; color: white;">Issues Traker</h1>
+    </div>
+</header>
+
+<main style="padding: 0 50px;">
+    <p><font size="9" color="#696969">New exception in enera_portal</font></p>
+    <br>
+    <p><font size="4" color="#000000">InvalidArgumentException</font>
+        <font size="4" color="#696969"> - vendor/laravel/framework/src/Illuminate/Database/DatabaseManager.php:238</font></p>
+    <p><font size="3" color="#696969">IDatabase [enera] not configured./font></p>
+    <br>
+    <p><font size="4" color="#696969">STAGE</font></p>
+    <p><font size="3" color="#696969">production</font></p>
+    <p><font size="4" color="#696969">SEVERITY</font></p>
+    <p><font size="" color="#696969">error</font></p>
+    <br>
+    <p><font size="9" color="#696969">Stacktrace summary</font></p>
+    <p style="border: solid #696969 1px; background-color: #f0f0f0; padding:5px 20px; margin:0;">app/Jobs/FbLikesJob.php:55 · [main]</p>
+    <p style="border: solid #696969 1px; background-color: #f0f0f0; padding:5px 20px; margin:0;">app/Jobs/FbLikesJob.php:55 · [main]</p>
+</main>
+
+</body>
+</html>
