0acf01b6322381915aa5f9073005c055e9b139f9
Pedro Luna <pluna@enera.mx>
2016-04-29 17:06:23 -0500

    [FIX] IssueTrackerHelper

--- resources/views/mail/issuestracker.blade.php
+++ resources/views/mail/issuestracker.blade.php
@@ -7,44 +7,45 @@
     <link rel="stylesheet" href="">
 </head>
 <body style="margin:0;">
-<header style="background-color: #00BFFF; padding: 20px; min-width: 500px;">
-    <div style="display: inline-block; margin-right: 20px;">
-        <img src="{{ asset('img/Logo Enera Blanco-01.svg') }}" alt="Enera" width="25">
-    </div>
-    <div style="display: inline-block;">
-        <h1 style="margin: 0 auto; color: white;">
-            New exception in Enera {{ $issue->issue['platform'] }}
-        </h1>
-    </div>
-</header>
+<div style="width: 100%">
+    <header style="background-color: #00BFFF; padding: 20px;">
+        <div style="display: inline-block; margin-right: 20px;">
+            <img src="{{ asset('img/Logo Enera Blanco-01.svg') }}" alt="Enera" width="25">
+        </div>
+        <div style="display: inline-block; text-align: center;">
+            <h1 style="margin: 0 auto; color: white;">
+                New exception in Enera {{ $issue->issue['platform'] }}
+            </h1>
+        </div>
+    </header>
 
-<main style="padding: 0 50px; min-width: 500px;">
-    <p>
-        <font size="9" color="#696969">
-            {{ $issue->issue['title'] }}
-        </font>
-    </p>
-    <br>
-    <p>
-        <font size="4" color="#000000">
-            {{ $issue->exception['msg'] }}
-        </font><br>
-        <font size="4" color="#696969">
-            {{ $issue->issue['file']['path'] }}:{{ $issue->issue['file']['line'] }}
-        </font>
-    </p>
-    <p>
-        <font size="3" color="#696969">
-            <b>ENTORNO</b> : {{ $env }}
-        </font><br>
-        <font size="3" color="#696969">
-            <b>HOST</b> : {{ gethostname() }}
-        </font><br>
-        <font size="3" color="#696969">
-            <b>URL</b> : ----
-        </font>
-    </p>
-</main>
-
+    <main style="padding: 0 50px; min-width: 500px;">
+        <p>
+            <font size="9" color="#696969">
+                {{ $issue->issue['title'] }}
+            </font>
+        </p>
+        <br>
+        <p>
+            <font size="4" color="#000000">
+                {{ $issue->exception['msg'] }}
+            </font><br>
+            <font size="4" color="#696969">
+                {{ $issue->issue['file']['path'] }}:{{ $issue->issue['file']['line'] }}
+            </font>
+        </p>
+        <p>
+            <font size="3" color="#696969">
+                <b>ENTORNO</b> : {{ $env }}
+            </font><br>
+            <font size="3" color="#696969">
+                <b>HOST</b> : {{ gethostname() }}
+            </font><br>
+            <font size="3" color="#696969">
+                <b>URL</b> : ----
+            </font>
+        </p>
+    </main>
+</div>
 </body>
 </html>
