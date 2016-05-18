51595476f2e1697078451c79e4d89d563491ecff
Eder Díaz <ederchrono@gmail.com>
2016-05-18 10:18:57 -0500

    player ooyala test

--- resources/views/welcome/ads.blade.php
+++ resources/views/welcome/ads.blade.php
@@ -12,6 +12,18 @@
         background-color: #3f51b5;
     }
 </style>
+
+
+<!-- ooyala code -->
+<link rel="stylesheet" type="text/css" href="../templates/product/videoplaza/miniplayer/style.css" />
+<script language="javascript">
+    function close_div_promotion() {
+        document.getElementById("div_promotion").innerHTML = "";
+        document.getElementById("div_promotion").style.display = "none";
+    }
+</script>
+
+
 @stop
 
 @section('header')
@@ -31,12 +43,76 @@
 <!-- Main card -->
 
 <!-- ad video -->
-<div class="card z-depth-2">
+<div class="card z-depth-2" style="width:557px; height:320px; margin-left: -278px; left:50%;">
 
-    <script src="//content.jwplatform.com/players/BYPLoEu3-p9g86TeQ.js"></script>
+    {{--<script src="//content.jwplatform.com/players/BYPLoEu3-p9g86TeQ.js"></script>--}}
     {{--<iframe style="margin-left: -240px; left: 50%; position: relative; margin-top: 15px;" src="//content.jwplatform.com/players/BYPLoEu3-p9g86TeQ.html" width="480" height="270" frameborder="0" scrolling="auto" allowfullscreen></iframe>--}}
 
+
+    <!-- ooyala video -->
+
+    <div id="div_promotion" class="ad-container">
+
+        <div class="mplayer">
+            <script language="javascript">
+                var vpMiniPlayer = window.vpMiniPlayer = {
+                    init : function() {
+
+                        // ****** START - videoplaza plugin config ******
+                        var queryString = "vphost=http://vp-validation.videoplaza.tv";
+                        queryString += "&vpTags=standard";
+                        queryString += "&vpCategory=validation";
+                        queryString += "&closeButtonTimeout=5";
+
+                        // ****** END - videoplaza plugin config ******
+
+                        var objid = "vpMiniPlayer_" + Math.floor(Math.random()*11000);
+                        var queryArray = queryString.split('&');
+                        var queryMap = {};
+                        for (var idx in queryArray) {
+                            var keyValue = queryArray[idx];
+                            var eqIdx = keyValue.indexOf('=');
+                            var key = keyValue.substring(0,eqIdx);
+                            var val = keyValue.substring(eqIdx + 1, keyValue.length);
+                            queryMap[key] = val;
+                        }
+                        if (!queryMap.width) {
+                            queryMap.width = 557;
+                        }
+                        if (!queryMap.height) {
+                            queryMap.height = 320;
+                        }
+                        if (!queryMap.showCountdownText) {
+                            queryMap.showCountdownText = "true";
+                        }
+                        if (!queryMap.closeButtonTimeout) {
+                            queryMap.closeButtonTimeout = "-1";
+                        }
+
+                        var miniplayerURL = "http://cdn.videoplaza.tv/resources/flash/miniplayer/latest/miniplayer.swf";
+
+                        var flashvars = 'key=value';
+                        for (var key in queryMap) {
+                            flashvars += "&" + key + '=' + queryMap[key];
+                        }
+
+                        document.write('<OBJECT classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" CODEBASE="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" ID="'+objid+'" WIDTH="' + queryMap.width + '" HEIGHT="' + queryMap.height + '"><PARAM NAME="movie" VALUE="'+miniplayerURL+'"><PARAM NAME="quality" VALUE="autohigh"><PARAM NAME="wmode" VALUE="opaque"><PARAM NAME="align" VALUE=""><PARAM NAME="FlashVars" VALUE="' + flashvars + '"><PARAM NAME="loop" VALUE="false"><PARAM NAME="allowScriptAccess" VALUE="always"><EMBED SRC="'+miniplayerURL+'" FlashVars="' + flashvars + '" NAME="'+queryMap.objid+'" quality="autohigh"  wmode="opaque" swLiveConnect="true" WIDTH="' + queryMap.width + '" HEIGHT="' + queryMap.height + '"    allowScriptAccess="always" TYPE="application/x-shockwave-flash" PLUGINSPAGE="http://www.macromedia.com/go/getflashplayer"></EMBED></OBJECT>');
+                    },
+
+                    prerollDone : function() {
+                        close_div_promotion();
+                    }
+                };
+
+                vpMiniPlayer.init();
+            </script>
+
+        </div>
+    </div>
+    <!-- end ooyala video -->
+
 </div>
+
 
 <div class="ads card small z-depth-2">
 
