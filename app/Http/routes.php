7ae0d448c23341b74ef11708ac2aaf1af1edfe08
Eder Díaz <ederchrono@gmail.com>
2016-08-26 11:56:28 -0500

    change of route to accept unifi

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,6 +12,7 @@
 */
 
 Route::match(['post', 'get'], '/', ['as' => 'welcome', 'uses' => 'WelcomeController@index']);
+Route::match(['post', 'get'], '/guest', ['as' => 'welcome', 'uses' => 'WelcomeController@index']);
 Route::match(['post', 'get'], '/ads', ['as' => 'ads', 'uses' => 'WelcomeController@ads']);
 
 Route::match(['post', 'get'], '/demo', ['as' => 'demo', 'uses' => 'DemoController@index']);
