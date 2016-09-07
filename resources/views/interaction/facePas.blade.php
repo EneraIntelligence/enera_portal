6b2185401ef76556e7e54b69e310c5e5c4557153
Eder Díaz <ederchrono@gmail.com>
2016-09-07 10:30:10 -0500

    analytics code inserted

--- resources/views/interaction/facePas.blade.php
+++ resources/views/interaction/facePas.blade.php
@@ -9,7 +9,7 @@
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-30029674-2', 'auto');
+    ga('create', 'UA-83818238-1', 'auto');
     ga('send', 'pageview');
 
 </script>
