5875fde695ffda33152b0ac70d6debd601c1d5a3
Eder Díaz <ederchrono@gmail.com>
2016-09-15 12:56:05 -0500

    merge of brand captcha

--- resources/views/interaction/brandCaptcha.blade.php
+++ resources/views/interaction/brandCaptcha.blade.php
@@ -69,7 +69,7 @@
                 <span class="white-text left">
                     Navegar en Internet
                 </span>
-                    <i class="right material-icons">wifi</i>
+                <i class="right material-icons">wifi</i>
             </a>
         </div>
 
@@ -80,10 +80,11 @@
 @section('footer_scripts')
     <script>
         var btn;
+        var clicked = false;
+
         $(document).ready(function () {
 
 
-            var clicked = false;
 
             var myLog = new logs();
 
@@ -92,88 +93,94 @@
                 client_mac: "{!! Input::get('client_mac') !!}"
             });
 
+            $("form").submit(function (e) {
+                e.preventDefault();
+                sendBrandCaptcha();
+            });
+
             btn = $(".nav-btn");
 
 
-            btn.click(function () {
-                if (!clicked) {
-                    //clicked = true;
-
-                    //json_data = JSON.stringify( $('form').serializeArray() );
-
-                    var o = {};
-                    var a = $('form').serializeArray();
-                    $.each(a, function() {
-                        if (o[this.name] !== undefined) {
-                            if (!o[this.name].push) {
-                                o[this.name] = [o[this.name]];
-                            }
-                            o[this.name].push(this.value || '');
-                        } else {
-                            o[this.name] = this.value || '';
-                        }
-                    });
-
-                    json_data=o;
-                    //alert(json_data);
-
-                    $.ajax({
-                        url: '/interaction/logs/brandcaptcha/',
-                        type: 'POST',
-                        dataType: 'JSON',
-                        data: json_data
-                    }).done(function (data)
-                    {
-                        console.log("response brandcaptcha");
-                        console.log(data);
-
-                        if(data.ok)
-                        {
-                            var success_url = btn.attr('success_url');
-
-                            //force redirect to ads
-                            var adsLink = "{{ URL::route('ads') }}";
-
-                            success_url = replaceUrlParam(success_url, "continue_url", adsLink);
-                            success_url = replaceUrlParam(success_url, "redir", adsLink);
-
-                            var completedJson = {
-                                _token: "{!! session('_token') !!}",
-                                client_mac: "{!! Input::get('client_mac') !!}"
-                            };
-
-                            myLog.completed(completedJson, function () {
-                                //on completed saved
-                                myLog.redirectOut(success_url);
-
-                            }, function () {
-                                //fail completed save
-                                myLog.redirectOut(success_url);
-                            });
-                        }
-                        else
-                        {
-                            alert("Captcha incorrecto");
-                            $( "#brand_cap_button_reload" )[0].click();
-                        }
-
-                    }).fail(function (jqXHR, textStatus, errorThrown)
-                    {
-                        console.log(jqXHR);
-                        console.log(textStatus);
-                        console.log(errorThrown);
-
-                        $( "#brand_cap_button_reload" )[0].click();
-
-                    });
-
-
-
-                }
-
-
-            });
+            btn.click(sendBrandCaptcha);
         });
+
+        function sendBrandCaptcha() {
+            if (!clicked) {
+                clicked = true;
+
+                //json_data = JSON.stringify( $('form').serializeArray() );
+
+                var o = {};
+                var a = $('form').serializeArray();
+                $.each(a, function () {
+                    if (o[this.name] !== undefined) {
+                        if (!o[this.name].push) {
+                            o[this.name] = [o[this.name]];
+                        }
+                        o[this.name].push(this.value || '');
+                    } else {
+                        o[this.name] = this.value || '';
+                    }
+                });
+
+                json_data = o;
+                //alert(json_data);
+
+                $.ajax({
+                    url: '/interaction/logs/brandcaptcha/',
+                    type: 'POST',
+                    dataType: 'JSON',
+                    data: json_data
+                }).done(function (data) {
+                    console.log("response brandcaptcha");
+                    console.log(data);
+
+                    if (data.ok) {
+                        var success_url = btn.attr('success_url');
+
+                        //force redirect to ads
+                        var adsLink = "{{ URL::route('ads') }}";
+
+                        success_url = replaceUrlParam(success_url, "continue_url", adsLink);
+                        success_url = replaceUrlParam(success_url, "redir", adsLink);
+
+                        var completedJson = {
+                            _token: "{!! session('_token') !!}",
+                            client_mac: "{!! Input::get('client_mac') !!}"
+                        };
+
+                        myLog.completed(completedJson, function () {
+                            //on completed saved
+                            myLog.redirectOut(success_url);
+
+                        }, function () {
+                            //fail completed save
+                            myLog.redirectOut(success_url);
+                        });
+                    }
+                    else {
+                        alert("Captcha incorrecto");
+                        $("#brand_cap_button_reload")[0].click();
+                        clicked=false;
+                    }
+
+                }).fail(function (jqXHR, textStatus, errorThrown) {
+                    console.log(jqXHR);
+                    console.log(textStatus);
+                    console.log(errorThrown);
+
+                    alert("Hubo un problema intenta de nuevo");
+
+                    $("#brand_cap_button_reload")[0].click();
+                    clicked=false;
+
+                });
+
+
+            }
+
+
+        }
 
         function replaceUrlParam(url, paramName, paramValue) {
             paramValue = encodeURIComponent(paramValue);
