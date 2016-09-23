4265018f44d6ef512c9bf2dda0968ed846e04b63
Eder Díaz <ederchrono@gmail.com>
2016-09-23 17:29:19 -0500

    changes on welcome so user have to login always

--- resources/views/interaction/like.blade.php
+++ resources/views/interaction/like.blade.php
@@ -69,7 +69,7 @@
         <div class="container like-container z-depth-2">
             <!-- like button -->
             <div class="fb-like" data-href="{{$like_url}}" data-send="false" data-layout="button" data-width="200"
-                 data-size="large" data-show-faces="false"></div>
+                 data-show-faces="false"></div>
         </div>
 
 
