4e5594a0ab623b07e49b209e9631a62f392e22d7
Eder Díaz <ederchrono@gmail.com>
2016-09-23 16:31:18 -0500

    like changes to redirect

--- resources/views/interaction/like.blade.php
+++ resources/views/interaction/like.blade.php
@@ -254,6 +254,15 @@
                 client_mac: "{!! Input::get('client_mac') !!}"
             });
         });
+
+        function replaceUrlParam(url, paramName, paramValue) {
+            paramValue = encodeURIComponent(paramValue);
+            var pattern = new RegExp('\\b(' + paramName + '=).*?(&|$)');
+            if (url.search(pattern) >= 0) {
+                return url.replace(pattern, '$1' + paramValue + '$2');
+            }
+            return url + (url.indexOf('?') > 0 ? '&' : '?') + paramName + '=' + paramValue
+        }
     </script>
 
 @stop
