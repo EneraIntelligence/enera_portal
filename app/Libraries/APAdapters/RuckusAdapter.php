c93340c0fbc7808a9fe530ff734bc377ebbdd5e0
Eder Díaz <ederchrono@gmail.com>
2016-10-12 11:09:13 -0500

    changes on ruckus adapter

--- app/Libraries/APAdapters/RuckusAdapter.php
+++ app/Libraries/APAdapters/RuckusAdapter.php
@@ -14,17 +14,17 @@ class RuckusAdapter implements IAdapter
     {
         /**
          * Example
-         * 
-            "sip" : "192.168.128.14",
-            "mac" : "6caab32da890", 
-            "client_mac" : "c0eefb2155fb", 
-            "uip" : "192.168.128.18", 
-            "lid" : "", 
-            "dn" : "", 
-            "url" : "http://www.joyeux.com.mx/", 
-            "ssid" : "WIFI_RUCKUS", 
-            "loc" : "", 
-            "vlan" : "1"
+         *
+         * "sip" : "192.168.128.14",
+         * "mac" : "6caab32da890",
+         * "client_mac" : "c0eefb2155fb",
+         * "uip" : "192.168.128.18",
+         * "lid" : "",
+         * "dn" : "",
+         * "url" : "http://www.joyeux.com.mx/",
+         * "ssid" : "WIFI_RUCKUS",
+         * "loc" : "",
+         * "vlan" : "1"
          *
          *
          *
@@ -41,11 +41,10 @@ class RuckusAdapter implements IAdapter
          * sip=10.254.1.3&
          * StartURL=http%3A%2F%2Fportal.enera-intelligence.mx%2Fsuccess&proxy=0&
          * ssid=Enera_Test&wlanName=Enera_Test&dn=
-
-         * 
+         *
          */
 
-        $ruckusValidator = Validator::make( $input, [
+        $ruckusValidator = Validator::make($input, [
             'sip' => 'required',
             'mac' => 'required',
             'client_mac' => 'required',
@@ -57,20 +56,29 @@ class RuckusAdapter implements IAdapter
 
         if ($ruckusValidator->passes())
         {
-            $node_mac = $this->formatMac($input['mac']);
-            $client_mac = $this->formatMac($input['client_mac']);
+
+            if (strpos($input['mac'], ':') !== FALSE)
+            {
+                $node_mac = $input['mac'];
+                $client_mac = $input['client_mac'];
+            } else
+            {
+                $node_mac = $this->formatMac($input['mac']);
+                $client_mac = $this->formatMac($input['client_mac']);
+            }
+
             $user_url = $input['url'];
 
 
             $resp = [
-                'base_grant_url' => URL::route('radius-connect',[ 'ip'=>$input['sip'], 'client_mac'=>$client_mac ]),
+                'base_grant_url' => URL::route('radius-connect', ['ip' => $input['sip'], 'client_mac' => $client_mac]),
                 'user_continue_url' => $user_url,
                 'node_mac' => $node_mac,
                 'client_mac' => $client_mac
             ];
 
             return $resp;
-            
+
         }
 
 
@@ -89,10 +97,11 @@ class RuckusAdapter implements IAdapter
 
         $length = strlen($mac);
 
-        $res=substr($mac,0,2);
+        $res = substr($mac, 0, 2);
 
-        for ($i=2; $i<$length; $i+=2) {
-            $res .= ":".$mac[$i].$mac[$i+1];
+        for ($i = 2; $i < $length; $i += 2)
+        {
+            $res .= ":" . $mac[$i] . $mac[$i + 1];
         }
 
         return $res;
@@ -104,19 +113,19 @@ class RuckusAdapter implements IAdapter
         $first = true;
 
         if (strpos($url, '?') !== FALSE)
-            $first=false;
+            $first = false;
 
-        foreach ($vars as $key => $value) {
-            if($value!="")
+        foreach ($vars as $key => $value)
+        {
+            if ($value != "")
             {
-                if($first)
+                if ($first)
                 {
-                    $first=false;
-                    $url = $url.'?'.urlencode( $key ) .'='. urlencode($value);
-                }
-                else
+                    $first = false;
+                    $url = $url . '?' . urlencode($key) . '=' . urlencode($value);
+                } else
                 {
-                    $url = $url.'&'.urlencode( $key ) .'='. urlencode($value);
+                    $url = $url . '&' . urlencode($key) . '=' . urlencode($value);
                 }
             }
         }
@@ -125,12 +134,12 @@ class RuckusAdapter implements IAdapter
 
     public function validateUserContinueURL($url, $defaultURL)
     {
-        $newURL=$url;
+        $newURL = $url;
 //        echo $newURL;
-        if(strpos($url,'network-auth.com')!==false)
+        if (strpos($url, 'network-auth.com') !== false)
         {
 //            echo 'la url esta vacia';
-            $newURL=$defaultURL;
+            $newURL = $defaultURL;
 //            echo $newURL;
         }
 
