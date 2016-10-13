45abce9b78cf3c128897df2e137d1ae38550a571
Eder Díaz <ederchrono@gmail.com>
2016-10-13 17:06:11 -0500

    testing connection with nbi via console

--- app/Console/Commands/NBITest.php
+++ app/Console/Commands/NBITest.php
@@ -75,8 +75,50 @@ class NBITest extends Command
         curl_close($curl);
 
         $response = json_decode($json_response, true);
-        $this->info("response: " . $json_response);
 
+        $this->info("Enc-mac: " . $response['Data']);
+        $encMac = $response['Data'];
+        //end mac
+
+        //start ip
+        $json_data = array(
+            "Vendor" => "ruckus",
+            "RequestPassword" => "t3!um123",
+            "APIVersion" => "1.0",
+            "RequestCategory" => "GetConfig",
+            "RequestType" => "Encrypt",
+            "Data" => "11:22:33:44"
+        );
+        $content = json_encode($json_data);
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER,
+            array("Content-type: application/json"));
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+
+        $json_response = curl_exec($curl);
+
+        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        if ($status != 200)
+        {
+            //error
+            die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+        }
+
+
+        curl_close($curl);
+
+        $this->info("Enc-ip: " . $response['Data']);
+        $encIp = $response['Data'];
+        //end ip
+
+
+
+        //start login
         $this->info("sending access request enc-mac: " . $response['Data']);
 
         $json_data = array(
@@ -85,7 +127,8 @@ class NBITest extends Command
             "APIVersion" => "1.0",
             "RequestCategory" => "UserOnlineControl",
             "RequestType" => "Login",
-            "UE-MAC" => $response['Data'],
+            "UE-IP" => $encMac,
+            "UE-MAC" => $encIp,
             "UE-Proxy" => "0",
             "UE-Username" => "test",
             "UE-Password" => "mal-password"
