6900cf66858125f0090ee693e88bf3256bb04f31
Eder Díaz <ederchrono@gmail.com>
2016-10-14 08:56:36 -0500

    refactor of nbi test

--- app/Console/Commands/NBITest.php
+++ app/Console/Commands/NBITest.php
@@ -51,34 +51,17 @@ class NBITest extends Command
             "RequestType" => "Encrypt",
             "Data" => "11:22:33:44"
         );
-        $content = json_encode($json_data);
 
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_HEADER, false);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HTTPHEADER,
-            array("Content-type: application/json"));
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
-
-        $json_response = curl_exec($curl);
-
-        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-        if ($status != 200)
-        {
-            //error
-            die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
-        }
-
-
-        curl_close($curl);
+        $json_response = $this->makeCurl($url, $json_data);
 
         $response = json_decode($json_response, true);
 
         $this->info("Enc-mac: " . $response['Data']);
         $encMac = $response['Data'];
         //end mac
+
+
+
 
         //start ip
         $json_data = array(
@@ -89,31 +72,12 @@ class NBITest extends Command
             "RequestType" => "Encrypt",
             "Data" => "172.21.134.101"
         );
-        $content = json_encode($json_data);
-
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_HEADER, false);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HTTPHEADER,
-            array("Content-type: application/json"));
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
-
-        $json_response = curl_exec($curl);
-
-        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-        if ($status != 200)
-        {
-            //error
-            die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
-        }
-
-
-        curl_close($curl);
+        $json_response = $this->makeCurl($url, $json_data);
+        $response = json_decode($json_response, true);
 
         $this->info("Enc-ip: " . $response['Data']);
         $encIp = $response['Data'];
+
         //end ip
 
 
@@ -134,6 +98,17 @@ class NBITest extends Command
             "UE-Username" => "test",
             "UE-Password" => "mal-password"
         );
+
+        $json_response = $this->makeCurl($url, $json_data);
+
+        $this->info("response: " . $json_response);
+
+
+    }
+
+
+    private function makeCurl($url, $json_data)
+    {
         $content = json_encode($json_data);
 
         $curl = curl_init($url);
@@ -156,8 +131,8 @@ class NBITest extends Command
 
 
         curl_close($curl);
-        $this->info("response: " . $json_response);
 
-
+        return $json_response;
     }
+
 }
