5938792192b078128228b3f68d664425b86625e2
Alfonso Rodriguez <darkdreke@gmail.com>
2016-11-11 17:03:47 -0600

    Merge branch 'master' into MultiVendor
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- app/Http/Controllers/PortalController.php
+++ app/Http/Controllers/PortalController.php
@@ -2,16 +2,21 @@
 
 namespace Portal\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 use Input;
 use MongoDate;
 use Portal\Branche;
+use Portal\Campaign;
 use Portal\CampaignLog;
 use Portal\Http\Requests;
 use Portal\Http\Controllers\Controller;
 use Portal\InputLog;
 use Portal\Libraries\APAdapters\APUtils;
+use Portal\Libraries\CampaignSelector;
+use Portal\Libraries\NewCampaignSelector;
+use Portal\User;
 
 class PortalController extends Controller
 {
@@ -27,31 +32,43 @@ class PortalController extends Controller
 
         $rawInput = Input::all();
 
-        if(env('APP_ENV')=='local')
-        {
+        if (env('APP_ENV') == 'local') {
             //simulating input for testing
             $rawInput['node_mac'] = "ac:86:74:50:48:e0";//concentro branch
-            $rawInput['base_grant_url'] = 'http:ederdiaz.com';
-            $rawInput['user_continue_url'] = 'http:ederdiaz.com';
-            $rawInput['client_mac'] = 'aa:bb:aa:bb:aa:bb';
+            $rawInput['base_grant_url'] = 'http://ederdiaz.com';
+            $rawInput['user_continue_url'] = 'http://ederdiaz.com';
+            $rawInput['client_mac'] = 'c0:ee:fb:21:55:fb';
         }
 
         //detecta marca de ap y asigna un adaptador
         $inputAdapter = APUtils::getAPAdapter($rawInput);
 
-        if ($inputAdapter == null)
-        {
+        if ($inputAdapter == null) {
             $this->saveInputLog($rawInput);
             return $this->invalidNetworkView();
         }
-
         //ajusta los inputs al estandar de enera
         $input = $inputAdapter->processInput($rawInput);
-
         session($input);
-        session(['campaign_id'=>'580e7414fd30b44734ebd6c7']);
+        $client_mac = $rawInput['client_mac'];
 
-        return view('welcome.portal', $this->getPortalData());
+        $user = User::where('facebook.id', 'exists', true)
+            ->whereIn('devices.mac', [$client_mac])
+            ->first();
+
+        if (!isset($user))
+            $campaignSelection = NewCampaignSelector::allUsers()->all();
+        else
+            $campaignSelection = NewCampaignSelector::facebookUsers($user['_id']);
+
+        $count = count($campaignSelection->all());
+        if ($count > 0) {
+            $campaign = $campaignSelection->all()[rand(0, $count - 1)];
+        } else {
+            $campaign = Campaign::where('_id', '5720fe8dc09c2fe0040041ac')->first();
+        }
+        session(['campaign_id' => $campaign['_id']]);
+        return view('welcome.portal', $campaign['original']['content'], $this->getPortalData());
 
 
     }
@@ -60,22 +77,21 @@ class PortalController extends Controller
     {
         //http://www.ffwd.mx/
 
-        $apGrantURL =session('base_grant_url');
+        $apGrantURL = session('base_grant_url');
 
-        if( isset( $apGrantURL ) )
-        {
+        if (isset($apGrantURL)) {
             $query = parse_url($apGrantURL, PHP_URL_QUERY);
 
 //            $bannerUrl = 'http://www.ffwd.mx/';
             $bannerUrl = url('ads');
             // Returns a string if the URL has parameters or NULL if not
             if ($query) {
-                $apGrantURL .= '&continue_url='.urlencode($bannerUrl) ;
+                $apGrantURL .= '&continue_url=' . urlencode($bannerUrl);
             } else {
-                $apGrantURL .= '?continue_url='.urlencode($bannerUrl) ;
+                $apGrantURL .= '?continue_url=' . urlencode($bannerUrl);
             }
 
-            $apGrantURL .= '&redir='.urlencode($bannerUrl) ;
+            $apGrantURL .= '&redir=' . urlencode($bannerUrl);
             $apGrantURL .= '&duration=1800';
 
             session(['success_redirect_url'=>$bannerUrl]);
@@ -85,8 +101,7 @@ class PortalController extends Controller
             $log = CampaignLog::where('user.session', session('_token'))
                 ->where('device.mac', $client_mac)->first();
 
-            if ($log)
-            {
+            if ($log) {
 
                 $log->interaction->accessed = new MongoDate();
                 $log->interaction->save();
@@ -94,11 +109,9 @@ class PortalController extends Controller
             }
 
             //dd($apGrantURL);
-            
+
             return redirect($apGrantURL);
-        }
-        else
-        {
+        } else {
             return redirect('portal');
         }
 
@@ -139,7 +152,7 @@ class PortalController extends Controller
             'message' => $branche->portal['message'],
             //'spinner_color' => $branche->portal['spinner_color'],
             //'login_response' => $this->fbUtils->makeLoginUrl($url),
-            'grant_access_url'=>url('grant_access'),
+            'grant_access_url' => url('grant_access'),
             'client_mac' => session('client_mac'),
         ];
     }
---
+++ app/Libraries/NewCampaignSelector.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ARodriguez
+ * Date: 11/9/16
+ * Time: 10:17
+ */
+
+namespace Portal\Libraries;
+
+
+use Carbon\Carbon;
+use DB;
+use MongoDate;
+use Portal\Campaign;
+use Portal\CampaignLog;
+use Portal\User;
+
+class NewCampaignSelector
+{
+
+    public static function allUsers()
+    {
+        $uniqueCampaign = self::uniqueTime(); //Campañas con interaccion unica por usuario
+        $uniquePerDay = self::uniqueTimePerDay(); //Campañas con interaccion unica por día
+        $maxInteractions = self::maxInteractions(); //campañas con limite de intercciones
+        $maxInteractionsPerDay = self::maxInteractionPerDay(); //campañas con limite de intercciones  por día
+        $weekday = self::campaignPerWeekDay(); //campañas filtardas por día de la semana
+        $hour = self::campaignPerHour(); //camapañas filtradas por hora del día
+        $period = self::periodTime(); //Verifica el periodo de vida de las campañas
+
+
+        $campaigns = Campaign::whereNotIn('_id', $uniqueCampaign)
+            ->whereNotIn('_id', $uniquePerDay)
+            ->whereNotIn('_id', $maxInteractions)
+            ->whereNotIn('_id', $maxInteractionsPerDay)
+            ->whereIn('_id', $weekday)
+            ->whereIn('_id', $hour)
+            ->whereIn('_id', $period)
+            ->where('status', 'active')
+            ->get();
+       
+        return $campaigns;
+    }
+
+    public static function facebookUsers($user_id)
+    {
+        $user = User::where('_id', $user_id)->first();
+        $today = Carbon::now();
+        $age = $today->diffInYears(Carbon::parse(date('Y-M-d h:i:s', $user['facebook']['age']->sec)));
+
+        $uniqueCampaign = self::uniqueTime(); //Campañas con interaccion unica por usuario
+        $uniquePerDay = self::uniqueTimePerDay(); //Campañas con interaccion unica por día
+        $maxInteractions = self::maxInteractions(); //campañas con limite de intercciones
+        $maxInteractionsPerDay = self::maxInteractionPerDay(); //campañas con limite de intercciones  por día
+        $weekday = self::campaignPerWeekDay(); //campañas filtardas por día de la semana
+        $hour = self::campaignPerHour(); //camapañas filtradas por hora del día
+        $period = self::periodTime(); //Verifica el periodo de vida de las campañas
+        
+        $campaigns = Campaign::whereNotIn('_id', $uniqueCampaign)
+            ->whereNotIn('_id', $uniquePerDay)
+            ->whereNotIn('_id', $maxInteractions)
+            ->whereNotIn('_id', $maxInteractionsPerDay)
+            ->whereIn('_id', $weekday)
+            ->whereIn('_id', $hour)
+            ->whereIn('_id', $period)
+            ->where('status', 'active')
+            ->whereIn('filters.gender', [$user['facebook']['gender']])
+            ->Where('filters.age.0', '<=', $age)->where('filters.age.1' , '>=', $age)
+            ->get();
+        
+        return $campaigns;
+    }
+
+    private static function uniqueTime()
+    {
+        $campaigns = Campaign::where('filters.unique_user', true)
+            ->where('status', 'active')->lists('_id');
+
+        $filter = CampaignLog::whereIn('campaign_id', $campaigns)
+            ->where('user.mac', session('client_mac'))
+            ->where('interaction.completed', 'exists', true)
+            ->lists('campaign_id');
+
+        return $filter->all();
+    }
+
+    private static function uniqueTimePerDay()
+    {
+        $today = Carbon::now();
+        $campaign_unique_per_day = Campaign::where('filters.unique_user', true)
+            ->where('status', 'active')->lists('_id');
+
+        $filter = CampaignLog::whereIn('campaign_id', $campaign_unique_per_day)->where('device.mac', session('client_mac'))
+            ->where('interaction.completed', new MongoDate(strtotime($today)))->lists('campaign_id');
+
+        return $filter->all();
+    }
+
+    private static function maxInteractions()
+    {
+        $campaign_max_interactions = Campaign::where('filters.max_interactions', '>', 0)
+            ->where('status', 'active')->lists('_id');
+
+        $filter = [];
+        foreach ($campaign_max_interactions->all() as $campaign_max) {
+            $count = CampaignLog::where('campaign_id', $campaign_max)
+                ->where('interaction.completed', 'exists', true)->count();
+
+            $campaign = Campaign::where('_id', $campaign_max)
+                ->where('filters.max_interactions', '<', $count)->first();
+            array_push($filter, $campaign['_id']);
+        }
+
+        return $filter;
+    }
+
+    private static function maxInteractionPerDay()
+    {
+        $today = Carbon::today();
+        $campaign_max_interaction_per_day = Campaign::where('filters.max_interactions_per_day', '>', 0)
+            ->where('status', 'active')->lists('_id');
+
+        $filter = [];
+        foreach ($campaign_max_interaction_per_day->all() as $campaign_max) {
+            $count = CampaignLog::where('campaign_id', $campaign_max)
+                ->where('interaction.completed', new MongoDate(strtotime($today)))->count();
+
+            $campaign = Campaign::where('_id', $campaign_max)
+                ->where('filters.max_interactions', '<', $count)->first();
+            array_push($filter, $campaign['_id']);
+        }
+
+        return $filter;
+
+    }
+
+    private static function campaignPerWeekDay()
+    {
+        $today = Carbon::today()->dayOfWeek;
+        $campaign_per_weekday = Campaign::whereIn('filters.week_days', [$today])
+            ->where('status', 'active')->lists('_id');
+
+        return $campaign_per_weekday->all();
+    }
+
+    private static function campaignPerHour()
+    {
+        $hour = Carbon::now()->hour;
+        $campaign_per_hour = Campaign::whereIn('filters.day_hours', [$hour])
+            ->where('status', 'active')->lists('_id');
+
+        return $campaign_per_hour->all();
+    }
+
+    private static function periodTime()
+    {
+        $today = Carbon::now();
+        $campaign = Campaign::where('filters.date.start', '<=', new MongoDate(strtotime($today)))
+            ->where('filters.date.end', '>=', new MongoDate(strtotime($today)))->where('status', 'active')->lists('_id');
+
+        return $campaign->all();
+    }
+}
