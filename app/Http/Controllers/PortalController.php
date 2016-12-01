63a4d70ed3aa0b572f88a4e4d5e58dc676b6e8f1
Alfonso Rodriguez <darkdreke@gmail.com>
2016-12-01 13:40:58 -0600

    dd

--- app/Http/Controllers/PortalController.php
+++ app/Http/Controllers/PortalController.php
@@ -66,7 +66,6 @@ class PortalController extends Controller
 
         $count = count($campaignSelection->all());
         if ($count > 0) {
-            dd($campaignSelection);
             $campaign = $campaignSelection->all()[rand(0, $count - 1)];
         } else {
             $campaign = Campaign::where('_id', '57e99662e24d83a6088b4579')->first();
